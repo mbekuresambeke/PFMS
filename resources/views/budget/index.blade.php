73eb172d3036b7366eef5474a8cf750e1ea6c525
Mbekure Sambeke <30600563+mbekuresambeke@users.noreply.github.com>
2022-11-11 18:39:52 +0300

    Commit From HP
    Today Commit

--- resources/views/budget/index.blade.php
+++ resources/views/budget/index.blade.php
@@ -7,13 +7,13 @@
             <div class="col-lg-8">
                 <div class="card">
                     <div class="card-header">
-                        <h2>Hello {{Auth::user()->name}}</h2>
+                        <h2>Hello {{ Auth::user()->name }}</h2>
                     </div>
                     <div class="card-body">
                         <p>
                             Your Currently Budget is
                         </p>
-                        <h2>{{number_format($Totalbudgets,2)}}</h2>
+                        <h2>{{ number_format($Totalbudgets, 2) }}</h2>
                     </div>
                 </div>
             </div>
@@ -25,35 +25,21 @@
                             <h2>Latest Budget</h2>
                         </div>
                         <div class="card-body">
-                            <h2>TSH: {{number_format($ThisMonthBudget,2)}}</h2>
+                            <h2>TSH: {{ number_format($ThisMonthBudget, 2) }}</h2>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
         <div class="row justify-content-center text-white ">
-            @foreach($budgets as $budget)
-            <div class="col-md-2 bg-primary  border shadow-2xl px-2 m-3 p-3 m-3  items-center ">
-                <h2>{{$budget->budget_title}}</h2>
-                <p>Amount: <small><strong>Tsh {{number_format($budget->budget_amount,2)}}</strong></small></p>
-                <p>Account Status is <code>{{$budget->budget_status ==1  ? "Active" : 0 =="inactive "}}</code></p>
-            </div>
+            @foreach ($budgets as $budget)
+                <div class="col-md-2 bg-primary  border shadow-2xl px-2 m-3 p-3 m-3  items-center ">
+                    <h2>{{ $budget->budget_title }}</h2>
+                    <p>Amount: <small><strong>Tsh {{ number_format($budget->budget_amount, 2) }}</strong></small></p>
+                    <p>Account Status is <code>{{ $budget->budget_status == 1 ? 'Active' : 0 == 'inactive ' }}</code></p>
+                </div>
             @endforeach
-            <div class="col-md-2 bg-primary  border shadow-2xl px-2 m-3 p-3 m-3  items-center ">
-                <h2>This Month Budget</h2>
-                <p>Amount: <small><strong>Tsh 300,000</strong></small></p>
-                <p>Account Status is <code>Bad</code></p>
-            </div>
-            <div class="col-md-2 bg-success  border shadow-2xl px-2 m-3 p-3 m-3  items-center ">
-                <h2>This Month Budget</h2>
-                <p>Amount: <small><strong>Tsh 300,000</strong></small></p>
-                <p>Account Status is <code>Not Bad</code></p>
-            </div>
-            <div class="col-md-2 bg-secondary  border shadow-2xl px-2 m-3 p-3 m-3  items-center ">
-                <h2>This Month Budget</h2>
-                <p>Amount: <small><strong>Tsh 300,000</strong></small></p>
-                <p>Account Status is <code>Not Bad</code></p>
-            </div>
+
         </div>
     </div>
 @endsection
